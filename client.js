088071092fd98800aae4c4c61850f31edec9d386
Monis Ahmed <monisahmed015@gmail.com>
2025-06-22 18:32:00 +0530

    Login and SignUp complete

--- client.js
+++ client.js
@@ -1,14 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
-import pg from "pg";
-import bcrypt from "bcrypt";
-import passport from "passport";
-import { Strategy } from "passport-local";
-import GoogleStrategy from "passport-google-oauth2";
-import session from "express-session";
 import env from "dotenv";
-import e from "express";
 
 const app = express();
 const port = 3000;
